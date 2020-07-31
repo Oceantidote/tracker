9fe7db85ab26a1b896573528b738e11b41ec425c
leonard percival <leonardpercival@gmail.com>
2020-07-31 15:39:49 +0100

    added bootsrap and some pages controller instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,13 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @projects = Project.includes(:tasks, :lists, :sessions).where(user: current_user, archived: false)
+    @invoices = Invoice.where(user: current_user)
   end
 
   def developer
+    @invoices = Invoice.all
+    @session = Session.where(end_time: nil)
+    @projects = Project.includes(:tasks, :lists, :sessions).where(archived: false)
   end
 end
