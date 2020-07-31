069b4ff18b0e88d0c0d798b72254b1f7d9e1943d
Ben Wright <ben1991wright@hotmail.co.uk>
2020-07-31 15:01:11 +0100

    set up basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :invoices
-  resources :tasks
-  resources :lists
-  resources :projects
   devise_for :users
+  resources :projects, except: [:destroy] do
+    resources :lists, only: [:new, :create]
+  end
+  resources :lists, except: [:new, :create, :destroy] do
+    resources :tasks, only: [:create]
+  end
+  resources :tasks, except: [:create, :new] do
+    # resources :sessions
+  end
+  resources :invoices
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
