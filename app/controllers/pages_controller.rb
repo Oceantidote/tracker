ffe0789c219757b29efc06caf707e66d260aadba
leonard percival <leonardpercival@gmail.com>
2020-07-31 15:20:13 +0100

    added seperate roots for clients and devs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,10 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+  end
+
+  def developer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  authenticate :user, ->(user) { user.admin? } do
+    root 'pages#developer', as: :admin_authenticated_root
+  end
+  authenticated :user do
+    root 'pages#dashboard', as: :authenticated_root
+  end
+  root to: 'pages#home'
   resources :invoices
   resources :tasks
   resources :lists
   resources :projects
   devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
