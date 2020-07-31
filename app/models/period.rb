bbb99a4c115bc06465e94ad2f847a51df3e69c65
Ben Wright <ben1991wright@hotmail.co.uk>
2020-07-31 16:06:26 +0100

    add method to calculate elapsed time

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,11 @@
 class Period < ApplicationRecord
   belongs_to :user
   belongs_to :task
+
+  def length
+    return if end_time.nil?
+
+    elapsed = (end_time - created_at) / 60.to_f
+    (elapsed / 5).ceil * 5
+  end
 end
