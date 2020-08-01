c9adddc3eda4c0aeb8edde5aaf231b317c90e31c
leonard percival <leonardpercival@gmail.com>
2020-08-01 10:52:06 +0100

    push to heroku

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,9 @@ class List < ApplicationRecord
   belongs_to :project
   has_many :tasks
   has_many :periods, through: :tasks
+
+  def incomplete_tasks
+    tasks.where(completed: false)
+  end
+
 end
