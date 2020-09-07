21736a39a307ef3efa0b1baec4e0fb9f3f77185b
leonard percival <leonardpercival@gmail.com>
2020-09-07 12:34:23 +0100

    done

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,3 +3,13 @@ class Note < ApplicationRecord
   belongs_to :user
   has_rich_text :content
 end
+
+
+
+class User
+  has_many :member_projects, through: :bookings, source: :project
+
+  def member_projects
+    self.bookings.map{|b| b.project}
+  end
+end
